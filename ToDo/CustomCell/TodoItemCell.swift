a3f3b682dfa646f707011e6b92f09770c0986c7d
codyByCherry <373466869@qq.com>
2018-11-08 21:39:57 +0800

    构建首页显示 简易的 todo item

---
+++ ToDo/CustomCell/TodoItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodoItemCell.swift
+//  ToDo
+//
+//  Created by Tony Zhang on 2018/11/08.
+//  Copyright © 2018年 SSM. All rights reserved.
+//
+
+import UIKit
+
+class TodoItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ ToDo/TodoListViewController.swift
@@ -0,0 +1,67 @@
+//
+//  TodoListViewController.swift
+//  ToDo
+//
+//  Created by Tony Zhang on 2018/11/08.
+//  Copyright © 2018年 SSM. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    let todoItems = ["go to school", "play ps4" ]
+    
+    let cellIdentifier = "TodoItemCell"
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupTableView()
+        
+    }
+    
+    
+    // MARK:- TODOS
+    
+    // TODO:: 1 构建自定义cell ToDoItemCell
+    func setupTableView() {
+        
+        let cellNib = UINib(nibName: "TodoItemCell", bundle: nil)
+        
+        self.tableView.register(cellNib, forCellReuseIdentifier: cellIdentifier)
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        
+        tableView.estimatedRowHeight = 100
+        
+        
+    }
+    // TODO:: 2 构建 Navigation Controller， Title ：ToDoey
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return todoItems.count
+        
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier:self.cellIdentifier, for: indexPath) as! TodoItemCell
+        
+        cell.textLabel?.text = todoItems[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    
+}
